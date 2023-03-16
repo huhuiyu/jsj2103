ba2665c7b1fa72a72dcc85945af920bcc3802dd1
huhuiyu <1069306849@qq.com>
2023-03-16 11:42:26 +0800

    2103班部门查询

--- website/info/js/dept.js
+++ website/info/js/dept.js
@@ -1,15 +1,36 @@
 import ajax from '../../js/ajax.js';
+import tools from '../../js/tools.js';
+
+console.log(tools.formatDate(new Date(), 'yyyy-MM-dd yyyy年'));
+console.log(tools.formatDate(new Date()));
 
 let tbDept = document.getElementById('tbDept');
+let txtDeptName = document.getElementById('txtDeptName');
+let btnQuery = document.getElementById('btnQuery');
+let btnReset = document.getElementById('btnReset');
 
 let dlist = [];
-let page = {};
+
+let page = {
+  pageNumber: 1,
+  pageSize: 5,
+};
+
+let queryInfo = {
+  deptName: '',
+};
 
 function queryDept() {
-  ajax.send('/manage/dept/queryAll', {}, (data) => {
+  let param = tools.concatJson(queryInfo, page);
+  console.log('合并的json:', param);
+
+  ajax.send('/manage/dept/queryAll', param, (data) => {
     if (data.success) {
       dlist = data.list;
       showDept();
+      // 分页信息更新为服务器端的返回值
+      page = data.page;
+      showPage();
       return;
     }
     alert(data.message);
@@ -18,6 +39,48 @@ function queryDept() {
 
 function showDept() {
   console.log('部门信息:', dlist, page);
+  tbDept.innerHTML = '';
+  for (let i = 0; i < dlist.length; i++) {
+    let dept = dlist[i];
+    let tr = document.createElement('tr');
+    let td;
+    // 部门编号
+    td = document.createElement('td');
+    td.append(dept.deptId);
+    tr.append(td);
+    // 部门编号
+    td = document.createElement('td');
+    td.append(dept.deptName);
+    tr.append(td);
+    // 部门信息
+    td = document.createElement('td');
+    td.append(dept.deptInfo);
+    tr.append(td);
+    // 信息最后修改时间
+    td = document.createElement('td');
+    td.append(tools.formatDate(dept.lastupdate));
+    tr.append(td);
+
+    tbDept.append(tr);
+  }
+}
+
+btnQuery.addEventListener('click', () => {
+  // 更新查询条件
+  queryInfo.deptName = txtDeptName.value;
+  // 切换到第一页
+  page.pageNumber = 1;
+  // 查询
+  queryDept();
+});
+
+btnReset.addEventListener('click', () => {
+  txtDeptName.value = '';
+  btnQuery.click();
+});
+
+function showPage() {
+  console.log('分页信息：', page);
 }
 
 queryDept();
---
+++ website/js/tools.js
@@ -0,0 +1,48 @@
+let tools = {
+  // 格式化日期
+  formatDate: (date, format = 'yyyy-MM-dd hh:mm:ss') => {
+    let time = new Date();
+    // 如果传入的是日期对象就直接赋值
+    if (typeof date == 'Date') {
+      time = date;
+    } else {
+      // 否则就是时间戳
+      time.setTime(date);
+    }
+    // 获取分区时间信息
+    let y = time.getFullYear();
+    let m = time.getMonth() + 1;
+    let d = time.getDate();
+    let h = time.getHours();
+    let mm = time.getMinutes();
+    let s = time.getSeconds();
+    // 格式化
+    let result = format.replace(/yyyy/g, y);
+    result = result.replace(/MM/g, m);
+    result = result.replace(/dd/g, d);
+    result = result.replace(/hh/g, h);
+    result = result.replace(/mm/g, mm);
+    result = result.replace(/ss/g, s);
+    return result;
+  },
+  // 合并多个json为一个，...是不定长参数
+  concatJson: (...jsons) => {
+    let result = {};
+    for (let i = 0; i < jsons.length; i++) {
+      const json = jsons[i];
+      // json对象的key循环
+      for (let key in json) {
+        // {id:100,name:'胡辉煜'}
+        // key=>id,name
+        // json['id'] ==> 100
+        console.log(key, json[key]);
+        // 复制信息
+        result[key] = json[key];
+      }
+    }
+
+    return result;
+  },
+};
+
+export default tools;
