5d198b5048323234199a4860ba71cddad9663ddf
huhuiyu <1069306849@qq.com>
2023-03-29 11:42:01 +0800

    2103班员工查询完成

--- website/info/js/employee.js
+++ website/info/js/employee.js
@@ -4,6 +4,12 @@ import tools from '../../js/tools.js';
 //#region 查询的部分=============
 let tbData = document.getElementById('tbData');
 
+let txtQName = document.getElementById('txtQName');
+let txtQPhone = document.getElementById('txtQPhone');
+let txtOrder = document.getElementById('txtOrder');
+let btnQuery = document.getElementById('btnQuery');
+let btnReset = document.getElementById('btnReset');
+
 let page = {
   pageNumber: 1,
   pageSize: 5,
@@ -19,6 +25,15 @@ let queryInfo = {
 };
 
 let elist = [];
+
+btnQuery.addEventListener('click', () => {
+  queryInfo.employeeName = txtQName.value;
+  queryInfo.phone = txtQPhone.value;
+  queryInfo.orderBy = txtOrder.value;
+
+  page.pageNumber = 1;
+  queryEmp();
+});
 
 function queryEmp() {
   let info = tools.concatJson(queryInfo, page);
@@ -111,6 +126,81 @@ links[3].addEventListener('click', () => {
   page.pageNumber = page.pageCount;
   queryEmp();
 });
+
+//#endregion
+
+//#region 部门选择的部分
+let dlist = [];
+let dpage = {
+  pageNumber: 1,
+  pageSize: 5,
+};
+let dquery = {
+  deptName: '',
+};
+
+let txtDept = document.getElementById('txtDept');
+let tbDept = document.getElementById('tbDept');
+let btnDept = document.getElementById('btnDept');
+
+let deptDialog = document.getElementById('deptDialog');
+let btnQueryDept = document.getElementById('btnQueryDept');
+let btnCloseDept = document.getElementById('btnCloseDept');
+
+btnDept.addEventListener('click', () => {
+  queryDept();
+  deptDialog.showModal();
+});
+
+btnCloseDept.addEventListener('click', () => {
+  // 触发表单提交（关闭对话框）
+  document.querySelector('#deptDialog form').submit();
+});
+
+function queryDept() {
+  dquery.deptName = txtDept.value;
+  let info = tools.concatJson(dquery, dpage);
+  ajax.send('/manage/dept/queryAll', info, (data) => {
+    if (data.success) {
+      dpage = data.page;
+      dlist = data.list;
+      showDept();
+      return;
+    }
+    alert(data.message);
+  });
+}
+
+function showDept() {
+  tbDept.innerHTML = '';
+
+  for (let i = 0; i < dlist.length; i++) {
+    const dept = dlist[i];
+    let tr = document.createElement('tr');
+    let td;
+    // 部门名称
+    td = document.createElement('td');
+    td.append(dept.deptName);
+    tr.append(td);
+    // 选择操作
+    td = document.createElement('td');
+    let button = document.createElement('button');
+    button.append('选择此部门');
+    button.addEventListener('click', () => {
+      // 修改查询条件
+      queryInfo.deptId = dept.deptId;
+      // 显示选中的部门
+      btnDept.innerHTML = `选中的部门：${dept.deptName}`;
+      // 关闭对话框
+      btnCloseDept.click();
+    });
+
+    td.append(button);
+    tr.append(td);
+
+    tbDept.append(tr);
+  }
+}
 
 //#endregion
 
