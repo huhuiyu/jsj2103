fd6f92dde8db543dc62c54a0df2f97ccc2d86b52
huhuiyu <1069306849@qq.com>
2023-04-13 11:32:39 +0800

    用户信息修改

--- website/user/js/main.js
+++ website/user/js/main.js
@@ -8,6 +8,7 @@ let userOtherInfo = {};
 
 let spUserName = document.getElementById('spUserName');
 let spUser = document.getElementById('spUser');
+let imgLogo = document.getElementById('imgLogo');
 
 function queryUserInfo() {
   ajax.send('/user/auth/getUserInfo', {}, (data) => {
@@ -40,11 +41,33 @@ function showUser() {
   } else if ('n' == tbUserInfo.sex) {
     sexn.checked = true;
   }
+
+  // 处理用户头像
+  if (tbUserInfo.img != '') {
+    imgLogo.setAttribute('src', tbUserInfo.img);
+  }
+  let sex = '保密';
+  if (tbUserInfo.sex == 'm') {
+    sex = '男神';
+  } else if (tbUserInfo.sex == 'f') {
+    sex = '女神';
+  }
+  // 显示信息的部分
+  divShowInfo.innerHTML = `
+    描述：${tbUserInfo.info}<br>
+    性别：${sex}<br>
+    QQ：${tbUserInfo.qq}<br>
+    微信：${tbUserInfo.wechat}<br>
+    手机号：${tbUserInfo.phone}<br>
+    邮箱：${tbUserInfo.email}<br>
+    注册天数：${userOtherInfo.regDays}天<br>
+  `;
 }
 
 //#endregion
 
 //#region 用户信息修改
+// https://service.huhuiyu.top/teach_project_service/user/file/download?fid=2
 let userDialog = document.getElementById('userDialog');
 let txtImg = document.getElementById('txtImg');
 let txtInfo = document.getElementById('txtInfo');
@@ -55,6 +78,50 @@ let sexm = document.getElementById('sexm');
 let sexf = document.getElementById('sexf');
 let sexn = document.getElementById('sexn');
 let btnSave = document.getElementById('btnSave');
+
+let divModify = document.getElementById('divModify');
+let divShowInfo = document.getElementById('divShowInfo');
+let btnChange = document.getElementById('btnChange');
+
+btnChange.addEventListener('click', () => {
+  if ('编辑' == btnChange.innerText) {
+    btnChange.innerText = '返回';
+    divShowInfo.style.display = 'none';
+    divModify.style.display = 'block';
+    btnSave.classList.remove('disabled');
+  } else {
+    btnChange.innerText = '编辑';
+    divShowInfo.style.display = 'block';
+    divModify.style.display = 'none';
+    btnSave.classList.add('disabled');
+  }
+});
+
+btnSave.addEventListener('click', () => {
+  let info = {
+    img: txtImg.value,
+    info: txtInfo.value,
+    nickname: txtNickname.value,
+    qq: txtQq.value,
+    wechat: txtWechat.value,
+    sex: 'n',
+  };
+  // 处理性别
+  if (sexm.checked) {
+    info.sex = 'm';
+  } else if (sexf.checked) {
+    info.sex = 'f';
+  }
+  // 保存修改
+  ajax.send('/user/auth/updateUserInfo', info, (data) => {
+    showToast(data.message);
+  });
+});
+
+// 关闭时要重新查询用户信息
+userDialog.addEventListener('hide.bs.modal', () => {
+  queryUserInfo();
+});
 
 //#endregion
 
