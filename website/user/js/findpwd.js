6bbc21bbf752cc2a572d9107dba568d9e1dbf7a9
huhuiyu <1069306849@qq.com>
2023-04-19 11:38:25 +0800

    找回密码

---
+++ website/user/js/findpwd.js
@@ -0,0 +1,19 @@
+//#region js导入
+
+import ajax from '../../js/ajax.js';
+import tools from '../../js/tools.js';
+import md5 from '../../libs/md5.min.js';
+
+//#endregion
+
+//#region 页面元素
+
+let txtName = document.getElementById('txtName');
+let txtCode = document.getElementById('txtCode');
+let btnCode = document.getElementById('btnCode');
+let txtPwd = document.getElementById('txtPwd');
+let btnOk = document.getElementById('btnOk');
+
+
+
+//#endregion
